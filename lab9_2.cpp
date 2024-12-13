edb0aa9398419573790f9eb6d0d5ad6cf58d23e1
Kquxz <firstkungza2005@gmail.com>
2024-12-13 17:13:17 +0700

    lab9

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 //Write the function printO() here
+void printO(int a,int b){
+	if(a<0 || b<0){
+		cout << "Invalid input";
+	}else{
+	for(int k = 1; k <= a; k++){
+		for(int m = 1; m <= b; m++){
+			cout << "O";
+		}
+		cout << "\n";
+	}
+	}
+	
+}
 
 int main(){
+	
+
 	
 	printO(2,2);
 	cout << "\n";
